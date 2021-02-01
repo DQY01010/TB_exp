4903de2c5816148f1d068ac8b84552e56a21fab0
DQY01010 <1076269100@qq.com>
2021-02-01 22:10:49 +0800

    process

--- process.py
+++ process.py
@@ -1,6 +1,7 @@
 from shutil import copyfile,copytree
 import numpy as np
-basepth = './BenMalData/data/'
+import os
+basepath = './BenMalData/data/RefineProcess/'
 phthisispath = './BenMalData/screenlist/phthisis.npy'
 hamartomapath = './BenMalData/screenlist/hamartoma.npy'
 inflammatory_pseudopath = './BenMalData/screenlist/inflammatory_pseudo.npy'
@@ -11,53 +12,61 @@ phlst = np.load(phthisispath)
 hamalst = np.load(hamartomapath)
 infllst = np.load(inflammatory_pseudopath)
 infelst = np.load(infectiouspath)
-chronlst = np.load(chronicTissueInflamspath)
+chronlst = np.load(chronicTissueInflampath)
 
 print(len(phlst),len(hamalst),len(infllst),len(infelst),len(chronlst))
 
 for i in phlst:
-#     print(i)
+    #print(i)
     casepth = i.split("#")[0]
     nodepth = i.split("#")[1]
-    datapth = basepath + casepth
+    datapth = os.listdir(basepath + casepth)
+    datanpypath = basepath + casepth
+    #print(datapth,casepth,nodepth)
     for j in datapth:
-        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth:
-            copyfile(datapth + "/" + j, "./BenMalData/phthisis/" + i)
+       # print(j)
+        #print(j.split("_")[0],j.split("_")[-1])
+        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth.split("_")[-1]:
+            copyfile(os.path.join(datanpypath,j), "./BenMalData/phthisis/" + i)
 
 for i in hamalst:
 #     print(i)
     casepth = i.split("#")[0]
     nodepth = i.split("#")[1]
-    datapth = basepath + casepth
+    datapth = os.listdir(basepath + casepth)
+    datanpypath = basepath + casepth
     for j in datapth:
-        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth:
-            copyfile(datapth + "/" + j, "./BenMalData/hamartoma/" + i)
+        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth.split("_")[-1]:
+            copyfile(os.path.join(datanpypath,j), "./BenMalData/hamartoma/" + i)
             
             
 for i in infllst:
 #     print(i)
     casepth = i.split("#")[0]
     nodepth = i.split("#")[1]
-    datapth = basepath + casepth
+    datapth = os.listdir(basepath + casepth)
+    datanpypath = basepath + casepth
     for j in datapth:
-        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth:
-            copyfile(datapth + "/" + j, "./BenMalData/inflammatory_pseudo/" + i)
+        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth.split("_")[-1]:
+            copyfile(os.path.join(datanpypath,j), "./BenMalData/inflammatory_pseudo/" + i)
             
                    
 for i in infelst:
 #     print(i)
     casepth = i.split("#")[0]
     nodepth = i.split("#")[1]
-    datapth = basepath + casepth
+    datapth = os.listdir(basepath + casepth)
+    datanpypath = basepath + casepth
     for j in datapth:
-        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth:
-            copyfile(datapth + "/" + j, "./BenMalData/infectious/" + i)   
+        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth.split("_")[-1]:
+            copyfile(os.path.join(datanpypath,j), "./BenMalData/infectious/" + i)   
             
 for i in chronlst:
 #     print(i)
     casepth = i.split("#")[0]
     nodepth = i.split("#")[1]
-    datapth = basepath + casepth
+    datapth = os.listdir(basepath + casepth)
+    datanpypath = basepath + casepth
     for j in datapth:
-        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth:
-            copyfile(datapth + "/" + j, "./BenMalData/chronicTissueInflam/" + i)            
+        if j.split("_")[0] == "DATA" and j.split("_")[-1] == nodepth.split("_")[-1]:
+            copyfile(os.path.join(datanpypath,j), "./BenMalData/chronicTissueInflam/" + i)            
