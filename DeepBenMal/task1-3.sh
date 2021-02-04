ba630e65ad991f08455fc7ef4b8be52d20550e7a
DQY01010 <1076269100@qq.com>
2021-02-04 12:53:51 +0800

    nothing

--- DeepBenMal/task1-3.sh
+++ DeepBenMal/task1-3.sh
@@ -1,19 +1,18 @@
 #/bin/bash 
 clear
-export CUDA_VISIBLE_DEVICES=0,1
+export CUDA_VISIBLE_DEVICES=0
 
-#  ============================================================================================================== 1:task1
-for valid_num in 0
+#  ============================================================================================================== 1:task1 
+for valid_num in 0 1 2 3 4
 #2 3 4
 do
-python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 224 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16  --lr 0.001 --num_valid $valid_num --save_dir dep34_crop96_sample224_dura48_aug4_notover_BCE --aug 4 --sample 'not-over' --task task1 --valid_path '../BenMalData/task/task1/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
+python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 224 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16  --lr 0.001 --num_valid $valid_num --save_dir dep34Pre_crop96_sample224_dura48_aug4_notover_BCE --aug 4 --sample 'not-over' --task task1 --valid_path '../BenMalData/task/task1/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
 done 
 
 #  ============================================================================================================== 2:task2
-for valid_num in 0
-#1 2 3 4
+for valid_num in 0 1 2 3 4
 do
-python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 224 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16 --lr 0.001 --num_valid $valid_num --save_dir dep34_crop96_sample224_dura48_aug4_notover_BCE --aug 4 --sample 'not-over' --task task2 --valid_path '../BenMalData/task/task2/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
+python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 224 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16 --lr 0.001 --num_valid $valid_num --save_dir dep34Pre_crop96_sample224_dura48_aug4_notover_BCE --aug 4 --sample 'not-over' --task task2 --valid_path '../BenMalData/task/task2/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
 done
 
 #  ============================================================================================================== 3:task3
