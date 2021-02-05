76716b01c1248ca6d78615b55cbe04cd7e93bdee
DQY01010 <1076269100@qq.com>
2021-02-05 23:47:44 +0800

    task

--- DeepBenMal/task1-3_32*96.sh
+++ DeepBenMal/task1-3_32*96.sh
@@ -12,7 +12,7 @@ export CUDA_VISIBLE_DEVICES=0
 for valid_num in 0 1 2 3 4
 #2 3 4
 do
-python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 96 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16  --lr 0.001 --num_valid $valid_num --save_dir dep34Pre_crop96_sample96_dura48_aug8_notover_BCE_wd2 --aug 8 --sample 'not-over' --task task1 --valid_path '../BenMalData/task/task1/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
+python TrainMalBegn.py --model 'resnet' --crop_size 96 --sample_size 96 --epochs 120 --n_classes 1 --sample_duration 48 --model_depth 34 --batch_size 16  --lr 0.001 --num_valid $valid_num --save_dir dep34Pre_crop96_sample96_dura48_aug8_notover_FP_wd2 --aug 8 --sample 'not-over' --task task1 --valid_path '../BenMalData/task/task1/class2_valid5' --pretrain_path './pretrained_model/resnet-34-kinetics.pth' --n_finetune_classes 1
 done
 
 #  ============================================================================================================== 3:task3
