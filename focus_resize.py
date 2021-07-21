89a9cf777fbe49bf02270b8f59a52cd52aa7b7d3
DQY01010 <1076269100@qq.com>
2021-07-21 19:19:17 +0800

    focus resize

--- focus_resize.py
+++ focus_resize.py
@@ -1,14 +1,20 @@
 import os
 import numpy as np
+import scipy
+from scipy.ndimage.interpolation import rotate
 
-if __name__ = "__main__":
-    path = '/home/DeepPhthisis/BenMalData/tb_210301_refine/'
-    dir_list = os.dir_list(path)
+if __name__ == "__main__":
+    path = '/home/DeepPhthisis/BenMalData/data/tb_210301_refine/'
+    dir_list = os.listdir(path)
     for i in dir_list:
         image_array = np.load(path+i)
         print(image_array.shape)
-        new_shape = [64,128,128]
-        real_resize_factor = new_shape / image_array.shape
-        new_spacing = spacing / real_resize_factor
-        image = scipy.ndimage.interpolation.zoom(image, real_resize_factor, mode='nearest')
-        np.save("/home/DeepPhthisis/BenMalData/data/TB_210301_resize/"+i,image)
+        real_resize_factor = [32/image_array.shape[0],64/image_array.shape[1],64/image_array.shape[2]]
+        image = scipy.ndimage.interpolation.zoom(image_array, real_resize_factor, mode='nearest')
+        label = np.load('/home/DeepPhthisis/BenMalData/lung_210301_label/' + i)
+        if label[0] == '1':
+            np.save("/home/DeepPhthisis/BenMalData/data/TB_210301_resize/sensitivity/"+i,image)
+        elif label[0] == '':
+            continue
+        else:
+            np.save("/home/DeepPhthisis/BenMalData/data/TB_210301_resize/resistant/"+i,image)
